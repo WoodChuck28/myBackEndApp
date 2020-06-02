961e14124273b1139ad441b3176cf7b1a5d772e4
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-06-02 07:16:17 -0400

    small

--- list.js
+++ list.js
@@ -3,7 +3,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: process.env.tableName2
+    TableName2: process.env.tableName2
     ,
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
